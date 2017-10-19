4d77f664b087171efb8b78936a6d7a60b166c539
Chris Chen <chris.jianfei.chen@gmail.com>
2017-10-19 20:06:21 +0800

    Minor revisions

--- scripts/run-experiments.py
+++ scripts/run-experiments.py
@@ -1,28 +1,34 @@
 import os, sys
 
-<<<<<<< HEAD
-datasets    = ['citeseer', 'cora', 'pubmed', 'nell', 'ppi']
-=======
-datasets    = ['reddit']
->>>>>>> eb4a659a13c4f7ae0867c67d7f6ba71cb9c9fe52
+datasets_runs    = [('citeseer', 10), ('cora', 10), ('pubmed', 10), ('nell', 10), ('ppi', 1), ('reddit', 1)]
 gcn_datasets = set(['cora', 'citeseer', 'pubmed', 'nell'])
 preprocess  = ['True', 'False']
 dropout = [True, False]
-deg_cv_dropout_preprocess   = [(20, False, 'True', True), (20, False, 'Fast', True), 
-                               (20, False, 'True', False), 
-                               (1, False, 'True', False), 
-                               (1, False, 'True', True), (1, False, 'Fast', True), 
-                               (1, True, 'True', True), (1, True, 'Fast', True)]
+#deg_cv_dropout_preprocess   = [(20, 'False', 'True', True), (20, 'False', 'Fast', True), 
+#                               (20, 'False', 'True', False), 
+#                               (1,  'False', 'True', False), 
+#                               (1,  'False', 'True', True), (1, 'False', 'Fast', True), 
+#                               (1,  'True', 'True', True), (1, 'True', 'Fast', True),
+#                               (1,  'TrueD','True', True)]
+
+deg_cv_dropout_preprocess   = [(1,  'TrueD','True', True)]
 
 f = open('run.sh', 'w')
-for data in datasets:
+for data, n_runs in datasets_runs:
     for deg, cv, d, pp in deg_cv_dropout_preprocess:
         if pp==False and data=='nell':
             continue
-        for run in range(3, 10):
+        for run in range(n_runs):
             dropout_str = ''
             if d=='Fast':
                 dropout_str = '--det_dropout'
+            if cv=='False':
+                cv_str = '--cv=False'
+            elif cv=='True':
+                cv_str = '--cv=True'
+            else:
+                cv_str = '--cv --cv2'
+
             log_file = 'logs/{}_pp{}_dropout{}_deg{}_cv{}_run{}.log'.format(data, pp, d, deg, cv, run)
             if data in set(['cora', 'citeseer', 'pubmed', 'nell']):
                 ndata  = 50000
@@ -35,5 +41,5 @@ for data in datasets:
                 epochs = 30
     
             command = \
-     'stdbuf -o 0 sh config/{}.config --early_stopping=1000000 --data={} --epochs={} {} --preprocess={} --degree={} --cv={} --test_cv={} --seed={} | tee {}'.format(data, ndata, epochs, dropout_str, pp, deg, cv, cv, run, log_file)
+     'stdbuf -o 0 sh config/{}.config --early_stopping=1000000 --data={} --epochs={} {} --preprocess={} --degree={} {} --seed={} | tee {}'.format(data, ndata, epochs, dropout_str, pp, deg, cv_str, run, log_file)
             f.write(command+'\n')
